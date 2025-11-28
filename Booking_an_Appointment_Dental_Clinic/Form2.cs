7f1d74a4c856037a7291fb8863026951a9d3e477
nooralsnwei <nooralsnwei@gmail.com>
2025-11-28 10:22:43 +0300

    Initial upload of dentel project

--- Booking_an_Appointment_Dental_Clinic/Form2.cs
+++ Booking_an_Appointment_Dental_Clinic/Form2.cs
@@ -14,6 +14,7 @@ namespace Booking_an_Appointment_Dental_Clinic
 {
     public partial class Book_an_appointment2 : Form
     {
+        //رفع
         public Book_an_appointment2()
         {
             InitializeComponent();
